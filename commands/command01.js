fd8e7124ebc798826b9899c8af0eb80e3c29894e
Anatolii Kosorukov <java1cprog@yandex.ru>
2025-10-23 20:17:39 +0700

    Export bot files (10 files)

---
+++ commands/command01.js
@@ -0,0 +1,10 @@
+/*CMD
+  command: /command01
+  help: 2222222222222222222
+  need_reply: false
+  auto_retry_time: 0
+CMD*/
+
+function() {
+    // Add your command logic here
+}
---
+++ commands/start1.js
@@ -0,0 +1,9 @@
+/*CMD
+  command: /start1
+  need_reply: false
+  auto_retry_time: 0
+CMD*/
+
+function() {
+    // Add your command logic here
+}
