c92077ffd72348bd572e57ecb56b041a11d62890
Anatolii Kosorukov <java1cprog@yandex.ru>
2025-10-23 08:43:42 +0700

    Sync: Update bot files (7 files)

---
+++ commands/newfolder013/start1.js
@@ -0,0 +1,15 @@
+/*CMD
+  id: 69535046
+  command: /start1
+  help:
+  need_reply: false
+  auto_retry_time: 0
+  folder: newfolder013
+  answer:
+  keyboard:
+  aliases:
+  updated_at: 2025-10-22T07:44:45.598Z
+  folder_id: 7657914
+  group:
+CMD*/
+
