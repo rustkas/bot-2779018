1d91d3f410c11bc632311cb5383f8d494dd93f64
Anatolii Kosorukov <java1cprog@yandex.ru>
2025-10-24 09:50:44 +0700

    Export bot files (8 files)

---
+++ commands/222/newcommand01.js
@@ -0,0 +1,15 @@
+/*CMD
+id: 69727365
+command: /newcommand01
+help: 
+need_reply: false
+auto_retry_time: 0
+folder: 222
+answer: 
+keyboard: 
+aliases: []
+group: 
+updated_at: 2025-10-24T02:50:12.893Z
+folder_id: 7657926
+CMD*/
+
---
+++ commands/new01.js
@@ -0,0 +1,13 @@
+/*CMD
+id: 69742559
+command: /new01
+help: 
+need_reply: false
+auto_retry_time: 0
+folder: 
+answer: 
+keyboard: 
+group: 
+updated_at: 2025-10-24T02:50:12.386Z
+CMD*/
+
